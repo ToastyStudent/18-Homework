8fb015476915e4e616f845b75efe33af88618db4
ToastyStudent <kanelayng2023@u.northwestern.edu>
2023-07-12 17:59:54 -0500

    Modified .gitignore and the connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,7 @@
+// Dependency of the Mongoose npm package
 const mongoose = require('mongoose');
 
+// Establishes connection to MongoDB database using Mongoose
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialmedia');
 
 module.exports = mongoose.connection;
