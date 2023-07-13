64f03f78441c3e3ce850ce24bb495b902e2520c7
ToastyStudent <kanelayng2023@u.northwestern.edu>
2023-07-12 21:07:01 -0500

    Modified the api routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -11,19 +11,21 @@ const {
 } = require('../../controllers/thought-controller');
 
 // The route at /api/thoughts is used to create a new thought when written to with a POST request, 
-// and to return all thoughts when accessed with a GET request.
+// and to return all thoughts when accessed with a GET request using their respective controller methods
+// imported from the thought-controller.js file above.
 router.route('/').get(getThoughts).post(createThought);
 
 // The route at /api/thoughts/:thoughtId is used for getting a single thought by its _id when accessed with a GET request,
-// updating a thought by its _id when accessed with a PUT request, and deleting a thought by its id when accessed with a DELETE request.
+// updating a thought by its _id when accessed with a PUT request, and deleting a thought by its id when accessed with a DELETE request
+// using their respective controller methods imported from the thought-controller.js file above.
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
 // The route at /api/thoughts/:thoughtId/reactions is used for creating a reaction stored in a single thought's reactions array field 
-// by that thought's _id when accessed with a POST request.
+// by that thought's _id when accessed with a POST request using the addReaction method imported from the thought-controller.js file above.
 router.route('/:thoughtId/reactions').post(addReaction);
 
 // /api/thoughts/:thoughtId/reactions/:reactionId is used for deleting a reaction by the reaction's reactionId 
-// value when accessed with a DELETE request.
+// value when accessed with a DELETE request using the removeReaction method imported from the thought-controller.js file above.
 router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
 
 module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,3 +1,4 @@
+// Dependencies
 const router = require('express').Router();
 const {
   getUsers,
@@ -9,13 +10,19 @@ const {
   removeFriend,
 } = require('../../controllers/user-controller');
 
-// /api/users
+// The route at /api/users is used to create a new user when written to with a POST request 
+// and to return all users when accessed with a GET request using their respective controller methods
+// imported from the user-controller.js file above.
 router.route('/').get(getUsers).post(createUser);
 
-// /api/users/:userId
+// /api/users/:userId is used for getting a single user by their _id when accessed with a GET request, 
+// updating a user by their _id when accessed with a PUT request, and deleting a user by their _id when accessed with a DELETE request
+// using their respective controller methods imported from the user-controller.js file above.
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
-// /api/users/:userId/friends/:friendId
+// /api/users/:userId/friends/:friendId is used for adding a new friend to a user's friend list when accessed with a POST request
+// and for removing a friend from a user's friend list when accessed with a DELETE request using their respective controller methods
+// imported from the user-controller.js file above.
 router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 module.exports = router;
