5d2c2e67fd16ccbb6b6aae686644b9c77435476f
ToastyStudent <kanelayng2023@u.northwestern.edu>
2023-07-12 21:20:05 -0500

    Modified the dateFormat.js file

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -1,9 +1,11 @@
 const addDateSuffix = (date) => {
   let dateStr = date.toString();
 
-  // get last char of date string
+  // Creates a new variable to hold the last character of the date string
   const lastChar = dateStr.charAt(dateStr.length - 1);
 
+  // Series of condtional statements that will check the last character of the date string 
+  // to determine if a suffix needs to be added and then adds it to the date string as needed
   if (lastChar === '1' && dateStr !== '11') {
     dateStr = `${dateStr}st`;
   } else if (lastChar === '2' && dateStr !== '12') {
@@ -17,12 +19,13 @@ const addDateSuffix = (date) => {
   return dateStr;
 };
 
-// function to format a timestamp, accepts the timestamp and an `options` object as parameters
+// Function that will format the timestamp to display the date, month, year, and time of day
 module.exports = (
   timestamp,
   { monthLength = 'short', dateSuffix = true } = {}
 ) => {
-  // create month object
+  // Creates an object to hold the months of the year with the key being the month number 
+  // with 0 being January and 11 being December
   const months = {
     0: monthLength === 'short' ? 'Jan' : 'January',
     1: monthLength === 'short' ? 'Feb' : 'February',
@@ -38,29 +41,35 @@ module.exports = (
     11: monthLength === 'short' ? 'Dec' : 'December',
   };
 
-  const dateObj = new Date(timestamp);
-  const formattedMonth = months[dateObj.getMonth()];
+  // Creates a new Date object based on the timestamp passed in
+  const dateObject = new Date(timestamp);
+  const formattedMonth = months[dateObject.getMonth()];
 
+  // Calls the addDateSuffix function to add a suffix to the date if needed
   const dayOfMonth = dateSuffix
-    ? addDateSuffix(dateObj.getDate())
-    : dateObj.getDate();
+    ? addDateSuffix(dateObject.getDate())
+    : dateObject.getDate();
 
-  const year = dateObj.getFullYear();
+  // Creates a variable to hold the year of the date object
+  const year = dateObject.getFullYear();
+  // Creates a variable to hold the hour of the date object and converts it from 24 hour time to 12 hour time
   let hour =
-    dateObj.getHours() > 12
-      ? Math.floor(dateObj.getHours() - 12)
-      : dateObj.getHours();
+    dateObject.getHours() > 12
+      ? Math.floor(dateObject.getHours() - 12)
+      : dateObject.getHours();
 
-  // if hour is 0 (12:00am), change it to 12
+  // Conditional statement that will set the hour to 12 if the hour is 0 i.e Midnight
   if (hour === 0) {
     hour = 12;
   }
 
-  const minutes = (dateObj.getMinutes() < 10 ? '0' : '') + dateObj.getMinutes();
+  // Creates a variable to hold the minutes of the date object and adds a 0 to the front if the minutes are less than 10
+  const minutes = (dateObject.getMinutes() < 10 ? '0' : '') + dateObject.getMinutes();
 
-  // set `am` or `pm`
-  const periodOfDay = dateObj.getHours() >= 12 ? 'pm' : 'am';
+  // Creates a variable to hold the period of the day i.e am or pm
+  const periodOfDay = dateObject.getHours() >= 12 ? 'pm' : 'am';
 
+  // Creates a variable to hold the fully formatted timestamp
   const formattedTimeStamp = `${formattedMonth} ${dayOfMonth}, ${year} at ${hour}:${minutes} ${periodOfDay}`;
 
   return formattedTimeStamp;
