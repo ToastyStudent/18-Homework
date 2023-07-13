3047b7013cf8dc728b1c512e22f223cad092bbc5
ToastyStudent <kanelayng2023@u.northwestern.edu>
2023-07-12 21:09:35 -0500

    Modified the toutes files

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const thoughtRoutes = require('./thought-routes');
 
-// Establishes the routes and prefixes them with the path /api
+// Establishes the routes and prefixes the user routes with /users and the thought routes with /thoughts
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
 
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
+// Dependencies
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+// Establishes the routes and prefixes the api routes with /api
 router.use('/api', apiRoutes);
 
+// Sends a message to the user if they try to access a route that doesn't exist
 router.use((req, res) => {
-  return res.send('Wrong route!');
+  return res.send('This route does not exist!');
 });
 
 module.exports = router;
