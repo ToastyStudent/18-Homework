c1b67e316e918c9562ed7bca80c0410cba56481f
ToastyStudent <kanelayng2023@u.northwestern.edu>
2023-07-12 21:22:30 -0500

    Modified the server.js file

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
+// Dependencies
 const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+// Establishes the port and starts the express.js server
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
+// Displays a message once the server is running
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+    console.log(`The API server is now running at port ${PORT}!`);
   });
 });
