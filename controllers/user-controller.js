02a43bf264651942b282d12172debf8ca9d5c601
ToastyStudent <kanelayng2023@u.northwestern.edu>
2023-07-12 20:24:50 -0500

    Added comments into the user-controller.js file

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,50 +1,59 @@
 const { User, Thought } = require('../models');
 
 const userController = {
-  // get all users
+  // An asyncrounous function that fetches all users
   async getUsers(req, res) {
+    // Attempt to retrieve all users from the database
     try {
-      const dbUserData = await User.find()
+      const databaseUserData = await User.find()
         .select('-__v')
 
-      res.json(dbUserData);
+      res.json(databaseUserData);
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
     }
   },
-  // get single user by id
+
+  // An asyncrounous function that fetches a single user using its id
   async getSingleUser(req, res) {
+    // Attempt to retrieve a single user from the database using the id provided by the user in the request parameters
     try {
-      const dbUserData = await User.findOne({ _id: req.params.userId })
+      const databaseUserData = await User.findOne({ _id: req.params.userId })
         .select('-__v')
         .populate('friends')
         .populate('thoughts');
 
-      if (!dbUserData) {
-        return res.status(404).json({ message: 'No user with this id!' });
+      // Conditional Statement that checks if there is a user with the given id, resulting in a 404 error if there is not
+      if (!databaseUserData) {
+        return res.status(404).json({ message: 'There is no user with this id in the database!' });
       }
 
-      res.json(dbUserData);
+      res.json(databaseUserData);
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
     }
   },
-  // create a new user
+
+  // An asyncrounous function that creates a new user
   async createUser(req, res) {
+    // Attempt to create a new user in the database using the data provided by the user in the request body
     try {
-      const dbUserData = await User.create(req.body);
-      res.json(dbUserData);
+      const databaseUserData = await User.create(req.body);
+      res.json(databaseUserData);
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
     }
   },
-  // update a user
+
+  // An asyncrounous function that updates a user by its id
   async updateUser(req, res) {
+    // Creates a variable that stores the user with the given id provided by the user in the request parameters
+    // Before updating the user's data
     try {
-      const dbUserData = await User.findOneAndUpdate(
+      const databaseUserData = await User.findOneAndUpdate(
         { _id: req.params.userId },
         {
           $set: req.body,
@@ -55,59 +64,70 @@ const userController = {
         }
       );
 
-      if (!dbUserData) {
-        return res.status(404).json({ message: 'No user with this id!' });
+      // Conditional Statement that checks if there is a user with the given id, resulting in a 404 error if there is not
+      if (!databaseUserData) {
+        return res.status(404).json({ message: 'There is no user in the database with this id!' });
       }
 
-      res.json(dbUserData);
+      res.json(databaseUserData);
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
     }
   },
-  // delete user (BONUS: and delete associated thoughts)
+
+  // An asyncrounous function that deletes a user by its id and its associated thoughts
   async deleteUser(req, res) {
+    // Attempt to delete a user from the database using the id provided by the user in the request parameters
     try {
-      const dbUserData = await User.findOneAndDelete({ _id: req.params.userId })
+      const databaseUserData = await User.findOneAndDelete({ _id: req.params.userId })
 
-      if (!dbUserData) {
-        return res.status(404).json({ message: 'No user with this id!' });
+      // A conditional statement that checks if there is a user with the given id, resulting in a 404 error if there is not
+      if (!databaseUserData) {
+        return res.status(404).json({ message: 'There is no user with this id in the database!' });
       }
 
-      // BONUS: get ids of user's `thoughts` and delete them all
-      await Thought.deleteMany({ _id: { $in: dbUserData.thoughts } });
-      res.json({ message: 'User and associated thoughts deleted!' });
+      // Deletes all thoughts associated with the deleted user using the id provided by the user in the request parameters
+      await Thought.deleteMany({ _id: { $in: databaseUserData.thoughts } });
+      res.json({ message: 'The user and their associated thoughts have been deleted' });
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
     }
   },
 
-  // add friend to friend list
+  // Asyncrounous function for adding a friend to a user's friend list
   async addFriend(req, res) {
+    // Attempt to add a friend to a user's friend list using the id of the other user provided by the user in the request parameters by
+    // storing that potential friend's id in a variable
     try {
-      const dbUserData = await User.findOneAndUpdate({ _id: req.params.userId }, { $addToSet: { friends: req.params.friendId } }, { new: true });
+      const databaseUserData = await User.findOneAndUpdate({ _id: req.params.userId }, { $addToSet: { friends: req.params.friendId } }, { new: true });
 
-      if (!dbUserData) {
-        return res.status(404).json({ message: 'No user with this id!' });
+      // Conditional Statement that checks if there is a user with the given id, resulting in a 404 error if there is not
+      if (!databaseUserData) {
+        return res.status(404).json({ message: 'There is no user in the database with this id!' });
       }
 
-      res.json(dbUserData);
+      res.json(databaseUserData);
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
     }
   },
-  // remove friend from friend list
-  async removeFriend(req, res) {
-    try {
-      const dbUserData = await User.findOneAndUpdate({ _id: req.params.userId }, { $pull: { friends: req.params.friendId } }, { new: true });
 
-      if (!dbUserData) {
-        return res.status(404).json({ message: 'No user with this id!' });
+  // Asyncrounous function for removing a friend from a user's friend list
+  async removeFriend(req, res) {
+    // Attempt to remove a friend from a user's friend list using the id of the other user provided by the user in the request parameters by
+    // storing that potential friend's id in a variable
+    try {
+      const databaseUserData = await User.findOneAndUpdate({ _id: req.params.userId }, { $pull: { friends: req.params.friendId } }, { new: true });
+
+      // Conditional Statement that checks if there is a user with the given id, resulting in a 404 error if there is not
+      if (!databaseUserData) {
+        return res.status(404).json({ message: 'There is no user in the database with this id!' });
       }
 
-      res.json(dbUserData);
+      res.json(databaseUserData);
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
